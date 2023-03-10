34aadc20e5d08ae4e920528b3d3607301bb9cd9e
vongocha <vongochapt@gmail.com>
2023-03-10 13:47:19 +0700

    new app

---
+++ applogin.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express!)
+const port = 3000
+app.getf/', (req, res) => {
+res.sendfXin moi chuyen gia dang nhap:')
+})
+app.listen(port, 0 => {
+console.log('App listening on port ${port}')
+
+
